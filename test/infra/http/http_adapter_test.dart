8dd62ccf18bffa13226cb020b9447bc0eb282647
Vicente Nascimento <vicente19981@live.com>
2022-03-21 22:01:05 -0300

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:ForDev/data/http/http.dart';
 import 'package:ForDev/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -72,6 +73,14 @@ void main() {
       mockResponse(204, body: '');
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
